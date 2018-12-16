5049669158cae212ebcecae02ade6ab5cfd9660b
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-12-16 13:23:15 +0100

    Use strictEqual instead of equal

--- test/index.js
+++ test/index.js
@@ -8,24 +8,24 @@ const { isValidEmail, getDomainPart, getLocalPart } = require('../index')
 describe('email-js', function () {
   describe('getLocalPart()', function () {
     it('returns the local part', function () {
-      assert.equal(getLocalPart('cake@example.org'), 'cake')
-      assert.equal(getLocalPart('cake.tower@gmail.com'), 'cake.tower')
+      assert.strictEqual(getLocalPart('cake@example.org'), 'cake')
+      assert.strictEqual(getLocalPart('cake.tower@gmail.com'), 'cake.tower')
     })
   })
 
   describe('getDomainPart()', function () {
     it('returns the domain', function () {
-      assert.equal(getDomainPart('cake@example.org'), 'example.org')
-      assert.equal(getDomainPart('cake.tower@gmail.com'), 'gmail.com')
+      assert.strictEqual(getDomainPart('cake@example.org'), 'example.org')
+      assert.strictEqual(getDomainPart('cake.tower@gmail.com'), 'gmail.com')
     })
   })
 
   describe('isValidEmail()', function () {
     it('validates the email', function () {
-      assert.equal(isValidEmail('cake@example.org'), true)
-      assert.equal(isValidEmail('root@localhost'), true)
-      assert.equal(isValidEmail('root'), false)
-      assert.equal(isValidEmail('@localhost'), false)
+      assert.strictEqual(isValidEmail('cake@example.org'), true)
+      assert.strictEqual(isValidEmail('root@localhost'), true)
+      assert.strictEqual(isValidEmail('root'), false)
+      assert.strictEqual(isValidEmail('@localhost'), false)
     })
   })
 })
